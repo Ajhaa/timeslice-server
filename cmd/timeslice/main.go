26974e016c97e1ed03c907f722c4f84a9eecd36f
atte <atte.h.1998@gmail.com>
2021-04-02 08:55:25 +0300

    dotenv, db connection from env

--- cmd/timeslice/main.go
+++ cmd/timeslice/main.go
@@ -2,33 +2,58 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"timeslice/internal/task"
 
 	"github.com/gin-gonic/gin"
-
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func main() {
-	r := gin.Default()
-
-	dsn := "host=localhost user= password= dbname= sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	err := godotenv.Load()
 
 	if err != nil {
-		fmt.Printf("Db error %s", err)
-		return
+		fmt.Println(err)
 	}
 
-	db.AutoMigrate(&task.Task{})
+	db := initDB()
+	r := gin.Default()
 
 	task.RegisterHandlers(r.Group("/tasks"), task.Service{Repo: task.Repository{DB: db}})
-	//
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 	r.Run()
+}
+
+func initDB() *gorm.DB {
+	host := envOrDefault("DB_HOST", "localhost")
+	port := envOrDefault("DB_PORT", "5432")
+	user := envOrDefault("DB_USER", "postgres")
+	password := envOrDefault("DB_PASSWORD", "")
+	dbname := envOrDefault("DB_NAME", "postgres")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.AutoMigrate(&task.Task{})
+
+	return db
+}
+
+func envOrDefault(key string, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
 	gorm.io/driver/postgres v1.0.8 // indirect
 	gorm.io/gorm v1.21.6 // indirect
 )
